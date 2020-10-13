ee0d7260b07c26d0f76e6fb6bb9a7dacc568e075
Giovane <giovaneaalmeida27@gmail.com>
2020-10-13 11:43:27 -0300

    Home and MiniPlayer components imported in App.js

--- App.js
+++ App.js
@@ -1,46 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 
 import React from "react";
-import { StyleSheet, ScrollView } from "react-native";
+import { StyleSheet, View } from "react-native";
 
-import List from "./components/List";
-
-let data = [
-  {
-    title: "Shuffle syndrome",
-    img: require("./assets/images/shuffle.jpg"),
-    id: 1,
-  },
-  {
-    title: "This is Dash Berlin",
-    img: require("./assets/images/dashBerlin.jpg"),
-    id: 2,
-  },
-  {
-    title: "Lofi Beats",
-    img: require("./assets/images/lofiBeats.jpg"),
-    id: 3,
-  },
-  {
-    title: "This Is Travis Scott",
-    img: require("./assets/images/travis.jpg"),
-    id: 4,
-  },
-  {
-    title: "Video Game Soundtracks",
-    img: require("./assets/images/videoGame.jpg"),
-    id: 5,
-  },
-];
+import Home from "./components/Home";
+import MiniPlayer from "./components/MiniPlayer";
 
 export default function App() {
   return (
-    <ScrollView style={styles.container}>
-      <StatusBar style="light" />
-      <List data={data} name="Tocadas recentemente" first={true} />
-      <List data={data} name="Escute de novo" />
-      <List data={data} name="Jazz in the background" />
-    </ScrollView>
+    <View style={styles.container}>
+      <StatusBar style="light" backgroundColor="#111" />
+      <Home />
+      <MiniPlayer />
+    </View>
   );
 }
 
